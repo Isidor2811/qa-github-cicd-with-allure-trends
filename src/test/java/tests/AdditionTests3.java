2bd384684b8812fdb6c8efef7ea01cff02144a21
olegdatsiuk <olegdatsiuk2811@gmail.com>
2023-04-06 10:50:27 +0300

    fixed suites

---
+++ src/test/java/tests/AdditionTests3.java
@@ -0,0 +1,25 @@
+package tests;
+
+import framework.PropertyLoader;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AdditionTests3 {
+
+    @Test
+    public void firstAdditionTest() {
+        Assert.assertEquals(5 + 5, 10, "Expected 5+5 to be equals to 10");
+        System.out.println(PropertyLoader.getProperty("BASE_URL"));
+    }
+
+    @Test
+    public void secondAdditionTest() {
+        Assert.assertEquals(5 + 1, 7, "Expected 5+1 to be equals to 7");
+    }
+
+    @Test
+    public void thirdAdditionTest() {
+        Assert.assertEquals(5 + 2, 7, "Expected 5+2 to be equals to 7");
+    }
+
+}
